fbf95cf4e0e48b970454a4cac89926cf7630876a
Joao Lino <linusmannuel@gmail.com>
2024-05-25 18:03:53 +0100

    feat: add nav bar and pedidos page

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="nav">
+    <nav>
+      <router-link to="/"> 
+        <img src="" alt="Logo">
+      </router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/pedidos">Pedidos</router-link>
+    </nav>
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
---
+++ src/views/Pedidos.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>Pedidos page</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Pedidos'
+  }
+</script>
